00bdd12fa414107d7a4afc94f375f88fec21f8f9
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-09-17 21:19:02 +0100

    added animation to the banner section with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './components/navbar'
 import Home from './components/home'
+import About from './components/about'
+import {gsap,Power3} from 'gsap'
 function App() {
-  return (
+  let tl = new gsap.timeline();
+  let  ease = Power3.easeOut();
+    return (
           <>
-        <Navbar/>
-        <Home/>
-      
+            <Navbar/>
+            <Home timeline={tl} ease={ease}/>
           </>
   )
 }
