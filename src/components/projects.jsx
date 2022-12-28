5d95714f385139fd63bab0c24b9fe50937c628df
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-12-28 21:13:44 +0100

    added another project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -31,10 +31,10 @@ export default function Projects(props){
             </div>
         </SwiperSlide>
         <SwiperSlide className='text-center'>
-        <h3 className="project-title text-4xl  project-title-3">OB</h3>
+        <h3 className="project-title text-4xl  project-title-3">Muse</h3>
         <div className='mt-4 flex flex-col md:flex-row btn-div'>
-                <a className='border-3  pl-4 pr-4 py-2 rounded-lg font-poppins demo-btn demo-btn-2 '  href='https://react-webpage-seven.vercel.app'>Live demo</a>
-                <a href='https://github.com/iceddrop/react-webpage' className='code-btn-2 text-black pl-4 pr-4 pt-2.5 font-poppins  rounded-lg code-btn'>Source code</a>
+                <a className='border-3  pl-4 pr-4 py-2 rounded-lg font-poppins demo-btn demo-btn-2 '  href='https://muse-liard.vercel.app'>Live demo</a>
+                <a href='https://github.com/iceddrop/muse' className='code-btn-2 text-black pl-4 pr-4 pt-2.5 font-poppins  rounded-lg code-btn'>Source code</a>
             </div>
         </SwiperSlide>
         <SwiperSlide className='text-center'>
